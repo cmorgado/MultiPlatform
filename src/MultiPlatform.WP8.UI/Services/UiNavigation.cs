032ca88f27763204fd0739ed75fad3cb9a70f9d7
cristovao morgado <cristovao.morgado@gmail.com>
2013-12-23 17:18:27 +0000

    update W8.1

---
+++ src/MultiPlatform.WP8.UI/Services/UiNavigation.cs
@@ -0,0 +1,123 @@
+﻿using MultiPlatform.Domain.Code;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+
+namespace MultiPlatform.WP8.UI.Services
+{
+    public class UiNavigation : Domain.Interfaces.INavigation<Domain.Interfaces.NavigationModes>
+    {
+        public static readonly Dictionary<Type, string> ViewModelRouting = new Dictionary<Type, string>
+                                                                    {
+                                                                          {
+                                                                              typeof(Domain.ViewModels.Home), "/Views/Home.xaml"
+                                                                          },
+                                                                          {
+                                                                              typeof(Domain.ViewModels.Details), "/Views/Details.xaml"
+                                                                          }
+                                                                          ,
+                                                                          {
+                                                                              typeof(Domain.ViewModels.Login), "/Views/Login.xaml"
+                                                                          }
+                                                                           ,
+                                                                          {
+                                                                              typeof(Domain.ViewModels.Map), "/Views/Map.xaml"
+                                                                          },
+                                                                           {
+                                                                              typeof(Domain.ViewModels.NFCSend), "/Views/Demo/NFCSend.xaml"
+                                                                          }
+                                                                          ,
+                                                                           {
+                                                                              typeof(Domain.ViewModels.SQLite), "/Views/Demo/SQLiteDemo.xaml"
+                                                                          }
+                                                                     };
+
+
+        void RootFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+
+
+            switch (e.NavigationMode)
+            {
+                case System.Windows.Navigation.NavigationMode.Back:
+                    navigationType = Domain.Interfaces.NavigationModes.Back;
+                    break;
+                case System.Windows.Navigation.NavigationMode.Forward:
+                    navigationType = Domain.Interfaces.NavigationModes.Forward;
+                    break;
+                case System.Windows.Navigation.NavigationMode.New:
+                    navigationType = Domain.Interfaces.NavigationModes.New;
+                    break;
+                case System.Windows.Navigation.NavigationMode.Refresh:
+                    navigationType = Domain.Interfaces.NavigationModes.Refresh;
+                    break;
+                case System.Windows.Navigation.NavigationMode.Reset:
+                    navigationType = Domain.Interfaces.NavigationModes.Refresh;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private Frame RootFrame
+        {
+            get { return Application.Current.RootVisual as Frame; }
+        }
+
+
+        public bool CanGoBack
+        {
+            get
+            {
+                return RootFrame.CanGoBack;
+            }
+        }
+
+
+        public void GoBack()
+        {
+            RootFrame.GoBack();
+        }
+
+
+        public void Navigate<TDestinationViewModel>(object parameter)
+        {
+            var navParameter = string.Empty;
+            if (parameter != null)
+            {
+                navParameter = "?param=" +  parameter.ToJson();
+            }
+
+            if (ViewModelRouting.ContainsKey(typeof(TDestinationViewModel)))
+            {
+                var page = ViewModelRouting[typeof(TDestinationViewModel)];
+
+                this.RootFrame.Navigate(new Uri("/" + page + navParameter, UriKind.Relative));
+            }
+        }
+
+        public void Navigate<TDestinationViewModel>(bool sameContext, object parameter)
+        {
+            if (sameContext)
+            {
+                // set animation
+            }
+            else
+            {
+                //set animation
+            }
+            Navigate<TDestinationViewModel>(parameter);
+        }
+
+
+
+        private Domain.Interfaces.NavigationModes navigationType;
+        public Domain.Interfaces.NavigationModes NavigationType { get { return navigationType; } }
+
+
+    }
+}
---
+++ src/MultiPlatform.WP8.UI/Views/UC/SimplePageTitle.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace MultiPlatform.WP8.UI.Views.UC
+{
+    public partial class SimplePageTitle : UserControl
+    {
+        public SimplePageTitle()
+        {
+            InitializeComponent();
+        }
+    }
+}
