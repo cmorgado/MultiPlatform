974326496275ddfab715cfa5099b21eb87c8ec81
cristovao morgado <cristovao.morgado@gmail.com>
2014-01-17 10:12:31 +0000

    handling screen size in Win8,1

--- src/MultiPlatform.W81.UI/Code/BasePage.cs
+++ src/MultiPlatform.W81.UI/Code/BasePage.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -10,6 +13,22 @@ namespace MultiPlatform.W81.UI.Code
 {
     public class BasePage : Page
     {
+        public BasePage()
+        {
+            this.Loaded += page_Loaded;
+            this.Unloaded += page_Unloaded;
+        }
+
+        private void page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Window.Current.SizeChanged += Window_SizeChanged;
+            DetermineVisualState();
+        }
+
+        private void page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Window.Current.SizeChanged -= Window_SizeChanged;
+        }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
@@ -37,6 +56,39 @@ namespace MultiPlatform.W81.UI.Code
             }
 
             base.OnNavigatedTo(e);
+        }
+
+        private void Window_SizeChanged(object sender, WindowSizeChangedEventArgs e)
+        {
+            DetermineVisualState();
+        }
+
+        private void DetermineVisualState()
+        {
+            var state = string.Empty;
+            var applicationView = ApplicationView.GetForCurrentView();
+            var size = Window.Current.Bounds;
+
+            if (applicationView.IsFullScreen)
+            {
+                if (applicationView.Orientation == ApplicationViewOrientation.Landscape)
+                    state = "FullScreenLandscape";
+                else
+                    state = "FullScreenPortrait";
+            }
+            else
+            {
+                if (size.Width == 320)
+                    state = "Snapped";
+                else if (size.Width <= 500)
+                    state = "Narrow";
+                else
+                    state = "Filled";
+            }
+
+            System.Diagnostics.Debug.WriteLine("Width: {0}, New VisulState: {1}", size.Width, state);
+
+            VisualStateManager.GoToState(this, state, true);
         }
     }
 }
